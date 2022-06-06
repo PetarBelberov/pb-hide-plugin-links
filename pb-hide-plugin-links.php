b11907a271e6432fd4b45a06cde0b430a3195658
PetarBelberov <petar_belberov@gmx.com>
2022-06-06 13:34:31 +0300

    refactoring: put all hooks in the init class method

--- pb-hide-plugin-links.php
+++ pb-hide-plugin-links.php
@@ -13,22 +13,24 @@ class HidePluginLinks
 {
     /**
      *
-     * Assign everything as a call from within the constructor
+     * Assign everything(the hooks) as a call from within the init method
      */
-    public function __construct()
+    public function init()
     {
-		add_filter( 'plugin_action_links', array($this, 'disable_plugin_deactivation' ), 10, 2);
+		add_filter( 'plugin_action_links', array($this, 'disable_plugin_deactivation_and_deletion' ), 10, 2);
     }
 
-	function disable_plugin_deactivation( $actions, $plugin_file ) {
+	function disable_plugin_deactivation_and_deletion( $actions, $plugin_file ) {
 		if (in_array( $plugin_file, array(
 		'category-ajax-filter/category-ajax-filter.php',
+		// Whatever plugin you want
 		)))
 
 		unset( $actions['deactivate'] );
 
 		if (in_array( $plugin_file, array(
 			'category-ajax-filter/category-ajax-filter.php',
+			// Whatever plugin you want
 		)))
 	
 		unset( $actions['delete'] );
@@ -38,3 +40,5 @@ class HidePluginLinks
 }
 
 $servicesListing = new HidePluginLinks();
+// Calling it init method outside of the class. The result is removing the coupling of the class to WordPress.
+$servicesListing->init();
